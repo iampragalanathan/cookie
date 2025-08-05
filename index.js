b7432e6b2a173859f8435d920768d2fb3f8f2882
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-05 11:02:40 +0530

    add

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-console.log(1)
+// // console.log(1)
 
-// setTimeout(function(){
-//     console.log(2)
-// } ,3000)
+// // // setTimeout(function(){
+// // //     console.log(2)
+// // // } ,3000)
 
-function runner(){
-    console.log(2)
-}
+// // function runner(){
+// //     console.log(2)
+// // }
 
-setTimeout(runner,2000)
-console.log(3)
+// // setTimeout(runner,2000)
+// // console.log(3)
+
+// function first(){
+
+//     console.log("first")
+// }
+
+// function third(){
+
+//     console.log("third")
+// }
+
+// function second(){
+//     console.log("second")
+// }
+
+// setTimeout(first,1500)
+// setTimeout(second,3000)
+// setTimeout(third,4500)
