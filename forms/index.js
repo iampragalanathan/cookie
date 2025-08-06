09f1c3976fbd3601326fc3c5e7f5f9be88617776
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-06 12:14:08 +0530

    change

--- forms/index.js
+++ forms/index.js
@@ -4,11 +4,17 @@ console.log("form refreshed the js")
 loginForm.addEventListener("submit",function(e){
 
     e.preventDefault()
-    console.log(e)
-    console.log(e.target)
-    console.log(e.target.id)
+    // console.log(e)
+    // console.log(e.target)
+    // console.log(e.target.id)
     const loginFormData=new FormData(loginForm)
     console.log(loginFormData)
+
+    const name=loginFormData.get("astronautName")
+    const email=loginFormData.get("astronautEmail")
+    const password=loginFormData.get("astronautPassword")
+
+    console.log(name,email,password)
 })
 
 
